c9177372cd770121051e404502cf7c06393251f7
IZERO_DESKTOP <izero3127@gmail.com>
2021-11-20 03:36:45 +0900

    gedaTools.py

--- gedaTools.py
+++ gedaTools.py
@@ -2,3 +2,7 @@
 def timeToInt(df):
     df = df['time'].apply(lambda x : int(x[2:]))
     return df
+
+# 데이터 프레임에서 country list 뽑아주는 메서드
+def countryM(df):
+    return list(set(map(lambda x: x[0], df.index)))
